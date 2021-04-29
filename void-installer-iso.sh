3f80375567e2fb13633b0e8af1aa881040d6b200
Dima-diep <52601543+Dima-diep@users.noreply.github.com>
2021-04-29 11:42:46 +0300

    void-installer-iso.sh

---
+++ void-installer-iso.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+echo "You've started installation Void on Termux"
+apt update -y
+apt upgrade -y
+pkg update -y
+pkg upgrade -y
+pkg install curl -y
+pkg install proot -y
+pkg install tar -y
+curl https://raw.githubusercontent.com/AndronixApp/AndronixOrigin/master/Installer/Void/void.sh | bash
+rm -rf void-installer-iso.sh
+chmod +x *
+./start-void.sh
+apt update -y
+apt upgrade -y
+apt install sudo -y
+sudo -i
+apt install python -y
+apt install python2 -y
+apt install python3 -y
+apt install git -y
+apt install wget -y
+apt install curl -y
+apt install pip -y
+apt install python-pip -y
+apt install python3-pip -y
+echo "Void has been installed"
