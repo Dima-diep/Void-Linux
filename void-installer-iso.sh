654e84ae8e80a93e176860f5ebd6ae8e67b36523
Dima-diep <52601543+Dima-diep@users.noreply.github.com>
2021-07-07 12:01:08 +0400

    void-installer-iso.sh

--- void-installer-iso.sh
+++ void-installer-iso.sh
@@ -7,21 +7,7 @@ pkg upgrade -y
 pkg install curl -y
 pkg install proot -y
 pkg install tar -y
+cd ~/
 curl https://raw.githubusercontent.com/AndronixApp/AndronixOrigin/master/Installer/Void/void.sh | bash
-chmod +x *
-./start-void.sh
-apt update -y
-apt upgrade -y
-apt install sudo -y
-sudo -i
-apt install python -y
-apt install python2 -y
-apt install python3 -y
-apt install git -y
-apt install wget -y
-apt install curl -y
-apt install pip -y
-apt install python-pip -y
-apt install python3-pip -y
 echo "Void has been installed"
-rm -rf void-installer-iso.sh
+rm -rf Void
